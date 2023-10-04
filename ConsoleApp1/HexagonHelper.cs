7505da751187b9434d2e164efbf4843fa349f59b
artofdarkness123 <artofdarkness123@gmail.com>
2023-10-03 21:36:47 -0400

    Add project files.

---
+++ ConsoleApp1/HexagonHelper.cs
@@ -0,0 +1,36 @@
+﻿namespace ConsoleApp1
+{
+    public class HexagonHelper
+    {
+        /// <summary>
+        /// The the area of a hgexagon given the diagonal
+        /// </summary>
+        /// <param name="diagonal">The distance from 1 120degree angle to another.</param>
+        /// <returns></returns>
+        public static decimal GetAreaGivenDiag(decimal diagonal)
+        {
+            return (3 * (decimal)Math.Sqrt(3) * (decimal)Math.Pow((double)diagonal, 2)) / 8;
+        }
+
+        /// <summary>
+        /// The the area of a hgexagon given the side
+        /// </summary>
+        /// <param name="side">the length of 1 side</param>
+        /// <returns></returns>
+        public static decimal GetSquareGivenSide(decimal side)
+        {
+            return ((3 * (decimal)Math.Sqrt(3)) / 2) * (decimal)Math.Pow((double)side, 2);
+        }
+
+        public static decimal GetSideFromArea(decimal area)
+        {
+            double temp = (double)(2 * (area / 9));
+            return (decimal)(Math.Pow(3, 0.25) * Math.Sqrt(temp));
+        }
+
+        //public static decimal GetSquare(decimal input)
+        //{
+        //    return input * input;
+        //}
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var area = HexagonHelper.GetAreaGivenDiag(12.5M);
+
+            Console.WriteLine(area);
+
+            Console.ReadLine();
+        }
+    }
+}
